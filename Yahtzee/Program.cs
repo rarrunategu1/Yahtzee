df923a4cebc39b30fef747e83d593ad79a7e407b
rarrunategu1 <parker.rose@ymail.com>
2019-08-19 19:00:23 -0400

    Add project files.

---
+++ Yahtzee/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Yahtzee
+{
+    public class Dice
+    {
+        Random random = new Random();
+        public int Roll()
+        {
+            return random.Next(1, 6);      
+        }
+
+        
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice dice = new Dice();
+            int numberRolled = dice.Roll();
+            Console.WriteLine(numberRolled);
+            
+        }
+    }
+}
