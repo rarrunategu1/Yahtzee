e634dafd936df1ac27c8c72fd04f4635e737aa07
rarrunategu1 <parker.rose@ymail.com>
2019-08-20 20:12:47 -0400

    refactored to include the option of rerolling the dice up to 3 times in the rollFiveDice method.

--- Yahtzee/Program.cs
+++ Yahtzee/Program.cs
@@ -6,54 +6,67 @@ namespace Yahtzee
     public class Dice
     {
         Random random = new Random();
+        List<int> numbersRolled = new List<int>(){0, 0, 0, 0, 0};
         public int Roll()
         {
             return random.Next(1, 7);      
         }
-        public List<int> rollFiveDice(string rollAgain)
-        {
-            List<int> numbersRolled = new List<int>();
+        public List<int> rollFiveDice()
+        {   
+            int rolls = 0;
 
-            for ( int i = 0; i < 5; i++)
+            while (true && rolls < 3)
+            {
+                Console.WriteLine("Would you like to roll all your dice?");
+                string reRoll = Console.ReadLine();
+
+                if (reRoll == "no")
                 {
-                    numbersRolled.Add(Roll());
-                    Console.WriteLine(numbersRolled[i]);
+                    break;
+                }
 
-                    if(rollAgain == "yes")
-                    {
+                else if (reRoll == "yes")
+                {
+                    rolls++;
+
+                    if (rolls == 1)
+                    { 
+
+                        for (int i = 0; i < 5; i++)
+                        {
+                            numbersRolled[i] +=Roll();
+                            Console.WriteLine(numbersRolled[i]);
+                        }
                         continue;
                     }
+                    else if (rolls > 1)
+                    {
+                        for (int i = 0; i < 5; i++)
+                        {
+                            numbersRolled = new List<int>(){0, 0, 0, 0, 0};
+                            numbersRolled[i] += Roll();
+                            Console.WriteLine(numbersRolled[i]);
+
+                        }
+                    }
                 }
-         
+            }
             return numbersRolled;
         }
-
-        
-
-        
-
-
     }
+
     class Program
     {
         static void Main(string[] args)
         {
             Dice dice = new Dice();
 
-            int rolls = 0;
+            dice.rollFiveDice();
+
+
             
-            while (true && rolls < 3)
-            {
-                Console.WriteLine("Would you like to roll your dice?");
-                string reRoll = Console.ReadLine();
-                
-                if (reRoll == "no")
-                {
-                    return;
-                }
-                dice.rollFiveDice(reRoll);
-                rolls++;
-            }
+
+
 
             
 
